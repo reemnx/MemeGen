c3071c91fac39a5f0e5a1e2785044a6602e1c86a
Reem Alon <37637982+reemnx@users.noreply.github.com>
2020-04-12 00:17:09 +0300

    Add files via upload

--- js/canvas-controller.js
+++ js/canvas-controller.js
@@ -4,9 +4,9 @@ var gCtx;
 var gPosX = 0;
 var gPosY = 0;
 var gIsLinePressed = false;
-var canvasTouchHandler = new Hammer(document.querySelector('#meme-editor-canvas'));
-var editorTouchHandler = new Hammer(document.querySelector('.editor-wraper'));
-var headerTouchHandler = new Hammer(document.querySelector('.header-container'));
+// var canvasTouchHandler = new Hammer(document.querySelector('#meme-editor-canvas'));
+// var editorTouchHandler = new Hammer(document.querySelector('.editor-wraper'));
+// var headerTouchHandler = new Hammer(document.querySelector('.header-container'));
 
 function initCanvas() {
     gCanvas = document.querySelector('#meme-editor-canvas');
@@ -47,43 +47,41 @@ document.querySelector('#meme-editor-canvas').addEventListener('mouseup', e => {
 
 // Touch Events
 
-canvasTouchHandler.on('panstart', function (e) {
-    e.preventDefault();
-    if (gCurrLine !== -1 || gCurrSticker !== -1) {
-        gIsLinePressed = true;
-        console.log(gIsLinePressed);
-        document.querySelector('body').style.touchAction = 'none' ;
-    }
-});
+// canvasTouchHandler.on('panstart', function (e) {
+//     e.preventDefault();
+//     if (gCurrLine !== -1 || gCurrSticker !== -1) {
+//         gIsLinePressed = true;
+//         console.log(gIsLinePressed);
+//         document.querySelector('html').style.touchAction = 'none' ;
+//     }
+// });
 
-canvasTouchHandler.on('pan' , function (e) {
-    e.preventDefault();
+// canvasTouchHandler.on('pan' , function (e) {
+//     e.preventDefault();
     
-    if (!gIsLinePressed) return;
+//     if (!gIsLinePressed) return;
 
-    if (gCurrLine !== -1) {
-        gMeme.lines[gCurrLine].posX = e.changedPointers[0].offsetX
-        gMeme.lines[gCurrLine].posY = e.changedPointers[0].offsetY + gMeme.lines[gCurrLine].lineHeight / 2;
-        editorImgDrawer(gMeme.imgUrl);
-    }
-    else {
-        gMeme.stickers[gCurrSticker].posX = e.changedPointers[0].offsetX - gMeme.stickers[gCurrSticker].width / 2;
-        gMeme.stickers[gCurrSticker].posY = e.changedPointers[0].offsetY - gMeme.stickers[gCurrSticker].height / 2;
-        editorImgDrawer(gMeme.imgUrl);
-    }
-});
-canvasTouchHandler.on('panend', function (e) {
-    e.preventDefault();
-    gIsLinePressed = false;
-});
-editorTouchHandler.on('pan press tap swipe', function (e) {
-    document.querySelector('body').style.touchAction = 'manipulation' ;
-});
-headerTouchHandler.on('pan press tap swipe', function (e) {
-    document.querySelector('body').style.touchAction = 'manipulation' ;
-});
-
-// document.querySelector('body').style.touchAction = 'manipulation' ;
+//     if (gCurrLine !== -1) {
+//         gMeme.lines[gCurrLine].posX = e.changedPointers[0].offsetX
+//         gMeme.lines[gCurrLine].posY = e.changedPointers[0].offsetY + gMeme.lines[gCurrLine].lineHeight / 2;
+//         editorImgDrawer(gMeme.imgUrl);
+//     }
+//     else {
+//         gMeme.stickers[gCurrSticker].posX = e.changedPointers[0].offsetX - gMeme.stickers[gCurrSticker].width / 2;
+//         gMeme.stickers[gCurrSticker].posY = e.changedPointers[0].offsetY - gMeme.stickers[gCurrSticker].height / 2;
+//         editorImgDrawer(gMeme.imgUrl);
+//     }
+// });
+// canvasTouchHandler.on('panend', function (e) {
+//     e.preventDefault();
+//     gIsLinePressed = false;
+// });
+// editorTouchHandler.on('pan press tap swipe', function (e) {
+//     document.querySelector('body').style.touchAction = 'manipulation' ;
+// });
+// headerTouchHandler.on('pan press tap swipe', function (e) {
+//     document.querySelector('body').style.touchAction = 'manipulation' ;
+// });
 
 //   Render canvas
 
---
+++ js/touchHandler.js
@@ -0,0 +1,43 @@
+'usr strict'
+
+var canvasTouchHandler = new Hammer(document.querySelector('#meme-editor-canvas'));
+var editorTouchHandler = new Hammer(document.querySelector('.editor-wraper'));
+var headerTouchHandler = new Hammer(document.querySelector('.header-container'));
+
+canvasTouchHandler.on('panstart', function (e) {
+    e.preventDefault();
+    console.log('panStart');
+    
+    if (gCurrLine !== -1 || gCurrSticker !== -1) {
+        gIsLinePressed = true;
+        document.querySelector('body').style.touchAction = 'none' ;
+    }
+});
+
+canvasTouchHandler.on('pan' , function (e) {
+    e.preventDefault();
+    document.querySelector('body').style.touchAction = 'none' ;
+    
+    if (!gIsLinePressed) return;
+
+    if (gCurrLine !== -1) {
+        gMeme.lines[gCurrLine].posX = e.changedPointers[0].offsetX
+        gMeme.lines[gCurrLine].posY = e.changedPointers[0].offsetY + gMeme.lines[gCurrLine].lineHeight / 2;
+        editorImgDrawer(gMeme.imgUrl);
+    }
+    else {
+        gMeme.stickers[gCurrSticker].posX = e.changedPointers[0].offsetX - gMeme.stickers[gCurrSticker].width / 2;
+        gMeme.stickers[gCurrSticker].posY = e.changedPointers[0].offsetY - gMeme.stickers[gCurrSticker].height / 2;
+        editorImgDrawer(gMeme.imgUrl);
+    }
+});
+canvasTouchHandler.on('panend', function (e) {
+    e.preventDefault();
+    gIsLinePressed = false;
+});
+editorTouchHandler.on('pan press tap swipe', function (e) {
+    document.querySelector('body').style.touchAction = 'manipulation' ;
+});
+headerTouchHandler.on('pan press tap swipe', function (e) {
+    document.querySelector('body').style.touchAction = 'manipulation' ;
+});
