afaf7691e680d09760ba9be01ab425b383278750
Reem Alon <37637982+reemnx@users.noreply.github.com>
2020-04-12 21:24:25 +0300

    Add files via upload

--- js/touchHandler.js
+++ js/touchHandler.js
@@ -4,19 +4,20 @@ var canvasTouchHandler = new Hammer(document.querySelector('#meme-editor-canvas'
 var editorTouchHandler = new Hammer(document.querySelector('.editor-wraper'));
 var headerTouchHandler = new Hammer(document.querySelector('.header-container'));
 
+
+
+canvasTouchHandler.on('pan press tap swipe', function (e) {
+    document.querySelector('body').style.touchAction = 'none' ;
+});
 canvasTouchHandler.on('panstart', function (e) {
-    e.preventDefault();
+    
     if (gCurrLine !== -1 || gCurrSticker !== -1) {
         gIsLinePressed = true;
-        document.querySelector('body').style.touchAction = 'none' ;
     }
 });
 canvasTouchHandler.on('pan' , function (e) {
-    e.preventDefault();
-    document.querySelector('body').style.touchAction = 'none' ;
     
     if (!gIsLinePressed) return;
-
     if (gCurrLine !== -1) {
         gMeme.lines[gCurrLine].posX = e.changedPointers[0].offsetX
         gMeme.lines[gCurrLine].posY = e.changedPointers[0].offsetY + gMeme.lines[gCurrLine].lineHeight / 2;
@@ -29,7 +30,6 @@ canvasTouchHandler.on('pan' , function (e) {
     }
 });
 canvasTouchHandler.on('panend', function (e) {
-    e.preventDefault();
     gIsLinePressed = false;
 });
 
